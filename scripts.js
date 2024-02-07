e606cc9dc9b999687931ce7aa59f757aceeeff3e
Anderson Martins <martins_xd@yahoo.com.br>
2024-02-07 12:05:36 -0300

    First Commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+
+
+const key = "eaafda2a4d4945c80b44947c8a073142"
+
+function colocarDados(dados) {
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
+    colocarDados(dados)
+}
+
+function btnClick() {
+    const cidade = document.querySelector(".input-city").value
+    buscarCidade(cidade)
+}
